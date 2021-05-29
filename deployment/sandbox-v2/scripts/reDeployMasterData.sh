3db75132e66d0ed0d2a24eac871ebdcc8d2200e5
Ankit Vaishnav <ankitvaishnav.iitb1991@gmail.com>
2021-05-29 20:11:23 +0530

    Updated masterData deployment script

--- deployment/sandbox-v2/scripts/reDeployMasterData.sh
+++ deployment/sandbox-v2/scripts/reDeployMasterData.sh
@@ -1,5 +1,7 @@
-#!/bin/sh
+#!/bin/bash
 
 rm -rf /home/mosipuser/mosip-infra/deployment/sandbox-v2/tmp/commons/ &&
 rm -rf /home/mosipuser/mosip-infra/deployment/sandbox-v2/tmp/mosip-ref-impl/ &&
 sudo rm /srv/nfs/mosip/postgres/init.lock
+cd /home/mosipuser/mosip-infra/deployment/sandbox-v2
+an playbooks/postgres.yml
